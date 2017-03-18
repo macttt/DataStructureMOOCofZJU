8bffce3dd7ec3ed87eab3aa1d332c128dba71d93
macttt <macttt@163.com>
2017-03-18 22:15:27 +0800

    通过
    succeed

--- EX2-2/EX2-2.c
+++ EX2-2/EX2-2.c
@@ -20,13 +20,14 @@ void Print( polyList L ){
 	if(NULL == L->next){
 		printf("0 0");
 	}else{
-		printf("%d",L->next->cft);
-		printf(" %d",L->next->index);
 		L = L->next;
-		while(L->next){
-			L = L->next;
-			printf(" %d",L->cft);
+		while(L){
+			printf("%d",L->cft);
 			printf(" %d",L->index);
+			L = L->next;
+			if(L){
+				printf(" ");
+			}
 		}
 	}
 	printf("\n");
@@ -34,19 +35,19 @@ void Print( polyList L ){
 
 //读取一个多项式链表第一个数字是项数，之后以系数和指数成对
 polyList read(){				 
-	polyList out,tmp;
+	polyList out,tmp,current;
 	out=(polyList)malloc(sizeof(polyList));		//结构体指针类型必须先要分配内存空间进行初始化，否则为野指针。 
 	tmp=(polyList)malloc(sizeof(polyList));
 	out->next=NULL;
 	out->cft=0;
 	out->index=0; 
 	tmp->next = out;
-	int in1,i,cft,index;
+	int in1=0,i=0,cft=0,index=0;
 	scanf("%d",&in1);
 	out->index=in1; 				//为了节省空间和方便操作，头结点的指数设置为多项式的项数。 
 	for(i=0;i<in1;i++){
 		scanf(" %d %d",&cft,&index);
-		polyList current=(polyList)malloc(sizeof(polyList));
+		current=(polyList)malloc(sizeof(polyList));
 		current->cft=cft;
 		current->index=index;
 		current->next=NULL;
@@ -56,66 +57,50 @@ polyList read(){
 	return out;
 }
 
-//对两个多项式进行乘法运算
-/*polyList multiply(polyList l1,polyList l2){
-	polyList add(polyList,polyList);			 
-	polyList result,currentadd,tmp,tmp2;
-	result=(polyList)malloc(sizeof(polyList));
-	current=(polyList)malloc(sizeof(polyList));
-	tmp=(polyList)malloc(sizeof(polyList));
-	tmp2=(polyList)malloc(sizeof(polyList));
-	result->next=NULL;
-	tmp->next=l1;
-	tmp->next=l2;
-	int i,j;
-	while(tmp->next->next){
-		tmp->next=tmp->next->next; 
-		while(tmp2->next->next){
-			tmp2->next=tmp2->next->next;
-			tmp->cft=tmp2->next->
-		}
+//单个节点与链表相乘
+polyList multipl(polyList element,polyList lst2){
+	polyList head,tmp1,nd;
+	head=(polyList)malloc(sizeof(polyList));
+	tmp1=(polyList)malloc(sizeof(polyList));
+	head->cft=0;
+	head->index=lst2->index;					//结果应该与l2项数相同 
+	tmp1->index=0;
+	tmp1->cft=0;
+	tmp1->next=head;
+	lst2=lst2->next;
+	while(lst2){
+		nd=(polyList)malloc(sizeof(polyList));
+		nd->cft=element->cft*lst2->cft;
+		nd->index=element->index+lst2->index;
+		nd->next=NULL;
+		tmp1->next->next=nd;
+		tmp1->next=tmp1->next->next;
+		lst2=lst2->next;
 	}
-	return result;
-}
-*/
+	return head;
+} 
 
 //用递归的方法对两个多项式进行乘法运算
 polyList multiply2(polyList l1,polyList l2){
 	polyList add(polyList,polyList);
-	polyList multiply2(polyList,polyList);			 
-	if(l1->next==NULL&&l2->next==NULL){
+	polyList multipl(polyList,polyList);
+	polyList result=(polyList)malloc(sizeof(struct node));
+	result->cft=0;
+	result->index=0;
+	result->next=NULL;
+	if(l1->index==0&&l2->index==0){
 		return l1;
-	}else if(l1->cft==0&&l1->next==NULL){
+	}else if(l1->index==0){
 		return l1; 
-	}else if(l2->cft==0&&l2->next==NULL){
+	}else if(l2->index==0){
 		return l2;
-	}else if(l1->next==NULL){
-		polyList current,tmp1,tmpl2;
-		tmp1=(polyList)malloc(sizeof(polyList));
-		tmpl2=(polyList)malloc(sizeof(polyList));
-		current=(polyList)malloc(sizeof(polyList));
-		tmp1->index=0;
-		tmp1->cft=0;
-		current->next=tmp1;
-		tmpl2->next=l2;
-		while(tmpl2->next->next){
-			tmpl2->next=tmpl2->next->next;
-			polyList nd;
-			nd=(polyList)malloc(sizeof(polyList));
-			nd->cft=l1->cft*tmpl2->next->cft;
-			nd->index=l1->index+tmpl2->next->index;
-			current->next->next=nd;
-			current->next=current->next->next;
-		}
-		return tmp1;
 	}else{
-		polyList tmp,ptrTmp;
-		ptrTmp=(polyList)malloc(sizeof(polyList));
-		tmp=(polyList)malloc(sizeof(polyList));
-		tmp->cft=l1->next->cft;
-		tmp->index=l1->next->index;
-		tmp->next=NULL;
-		return add(multiply2(tmp,l2),multiply2(l1->next,l2));
+		l1=l1->next;
+		while(l1){
+			result=add(result,multipl(l1,l2));
+			l1=l1->next;
+		}
+		return result;
 	} 
 }
 
@@ -134,6 +119,8 @@ polyList add(polyList l1,polyList l2){
 		if(l1->next->index==l2->next->index){
 			current->cft=l1->next->cft+l2->next->cft;
 			if(current->cft==0){
+				l1=l1->next;
+				l2=l2->next;
 				continue;
 			}else{
 				current->index=l1->next->index;
@@ -168,10 +155,10 @@ polyList add(polyList l1,polyList l2){
 	return result;
 }
 
-int main(int argc, char *argv[]) {
-	polyList multiplyout,addout,l1,l2;
-	multiplyout=(polyList)malloc(sizeof(polyList));
-	addout=(polyList)malloc(sizeof(polyList));
+int main(){
+	polyList l1,l2;
+	void Print(polyList);
+	polyList add(polyList,polyList);
 	l1=(polyList)malloc(sizeof(polyList));
 	l2=(polyList)malloc(sizeof(polyList)); 
 	l1=read();
